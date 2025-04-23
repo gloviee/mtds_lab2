215fb49b2d167a46a5f271d15a37bef661888765
exluse <krasulyaalecander@gmail.com>
2025-04-23 15:25:25 +0300

    Revert "Тест, який навмисно падає (для CI)"
    This reverts commit cf1f08fd66a40173ade7f5628471753340eb76a0.

--- test_array_list.py
+++ test_array_list.py
@@ -25,9 +25,6 @@ class TestArrayList(unittest.TestCase):
         lst.append('z')
         copy = lst.clone()
         self.assertEqual(str(copy), 'z')
-    
-    def test_fail_example(self):
-        self.assertEqual(1, 2)
 
 if __name__ == '__main__':
     unittest.main()
